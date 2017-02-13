1c237f930cbb46b3623dfd253dc7dc6b64ccdc5c
Dark Arts Ninja <danixue@gmail.com>
2017-02-12 22:19:21 -0500

    Update introJavaScriptLoops.js

--- introJavaScriptLoops.js
+++ introJavaScriptLoops.js
@@ -5,10 +5,7 @@ var totalMW = 0;
 while (currentGen <= 4) {
   
   totalMW = totalMW + 62;
-  
-  
-  	console.log("Generator #" + (currentGen) + " is on, adding 62 MW, for a total of " + totalMW + " MW!");
-  
+  console.log("Generator #" + (currentGen) + " is on, adding 62 MW, for a total of " + totalMW + " MW!");
   currentGen++;
   
 }
@@ -17,7 +14,6 @@ while (currentGen <= 4) {
   currentGen++){
     
   totalMW = totalMW + 124;
-  
   console.log("Generator #" + currentGen + " is on, adding 124 MW, for a total of " + totalMW + " MW!");
   
 }
